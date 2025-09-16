09003ae87cb55053746649871f4c618087c19337
okzScripts <oskar.krantz90@gmail.com>
2025-09-16 11:47:01 +0200

    added sqlite db and write function to route

--- api.py
+++ api.py
@@ -4,12 +4,44 @@ import joblib
 import numpy as np
 import os
 
+import sqlite3
+
 app = Flask(__name__)
 CORS(app)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 model = joblib.load(os.path.join(current_dir, "models/random_forest_crop.joblib"))
 scaler = joblib.load(os.path.join(current_dir, "models/scaler.joblib"))
+
+def get_connection():
+    conn = sqlite3.connect("predicitons.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def Write_to_db(data, prediction):
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+                INSERT INTO predictions (N, P, K, temperature, humidity, ph, rainfall, predicted_yield, crop)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """, (
+                data['N'],
+                data['P'],
+                data['K'],
+                data['temperature'],
+                data['humidity'],
+                data['ph'],
+                data['rainfall'],
+                prediction         
+            ))
+        conn.commit()
+        conn.close()
+        return True
+    except Exception as e:
+        print(f"db error : {e}")
+        return False
+
 
 @app.route("/predict", methods=['POST'])
 def predict():
@@ -24,10 +56,14 @@ def predict():
         features_scaled = scaler.transform(features)
         prediction = model.predict(features_scaled)
 
+        Write_to_db(data,prediction)
+
         return jsonify({
             'prediction': prediction[0],
             'status': "success"
         })
+        
+
     except Exception as e:
         return jsonify({
             "error": str(e),
